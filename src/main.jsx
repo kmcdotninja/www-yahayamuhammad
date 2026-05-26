91d7fc42efaaa70c78af2d72406f73c5de073be9
kmcdotninja <yahayabinmuhammad@gmail.com>
2026-05-26 14:45:55 +0100

    chore: analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 
@@ -27,5 +28,6 @@ document.documentElement.classList.add('is-loading')
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
